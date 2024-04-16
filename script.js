04d4724ad864ae842739458da96bb80dbcfc2afc
Dayan Qazi <dayanqazi622@gmail.com>
2024-04-16 10:43:47 +0500

    Work Done

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const turnon =()=>{
+    let imgob=document.getElementById("bulb");
+    imgob.setAttribute("src", "https://img.freepik.com/premium-photo/incandescent-heart-shaped-light-bulb-black-background-valentine-s-day_180633-1262.jpg?w=740");
+};
+
+const turnoff =()=>{
+    let imgob=document.getElementById("bulb");
+    imgob.setAttribute("src", "https://img.freepik.com/premium-photo/light-bulb-bulb-concept-express-idea-isolated-black-background-with-space-text_636935-492.jpg?w=740");
+};
